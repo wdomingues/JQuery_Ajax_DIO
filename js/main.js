ae73ed595e4a25f9b213fe79740a5644b331cba8
Winne Domingues <wdomingues@id.uff.br>
2020-07-02 04:08:37 -0300

    2. Desenvolvendo jQuery com Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-alert("funcionou!");
-
 function consultaCEP(){
+    $(".barraProgresso").show();
     var cep = document.getElementById("cep").value;
     var endereco = "http://viacep.com.br/ws/" + cep + "/json/";
+    console.log(endereco);
     $.ajax({
         url: endereco,
         type: "GET",
         success: function(response){
+
             console.log(response);
+
             //document.getElementById("logradouro").innerHTML = response.logradouro;
             //ou
             $("#logradouro").html(response.logradouro);
@@ -15,7 +17,12 @@ function consultaCEP(){
             document.getElementById("bairro").innerHTML = response.bairro;
             document.getElementById("localidade").innerHTML = response.localidade;
             document.getElementById("uf").innerHTML = response.uf;
-
+            $("#titulo").html("CEP: "+response.cep);
+            $(".cep").show();
         }
     })
 }
+$(function(){
+    $(".cep").hide();
+    $(".barraProgresso").hide();
+});
